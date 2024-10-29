ec57bdfa879c3c88a9ed5365eaf614b569d3ea9e
Mohammed Almethen <almethen2002@gmail.com>
2024-10-29 21:32:44 +0300

    Added index.js for BMI task

--- index.js
+++ index.js
@@ -7,5 +7,29 @@ function calculateBMI() {
   let female = document.getElementById("f").checked;
   console.table({ age, weight, height, male, female });
 
-  // You are going to write the code here before the curly braces 👇🏻
-}
+  
+  const BMI = (weight / ((height /100) * (height /100))); // BMI Calculator  
+  alert(BMI); // Showing result as an alert 
+
+  // Challenge 2 //
+  /* if (BMI < 18.5) alert("Your Underweight"); 
+ else if (BMI >= 18.5 && BMI <= 25.0)  alert("Healthy Weight"); 
+ else if (BMI >= 25.0 && BMI < 30.0) alert("Overweight");
+ else if  (BMI > 30.0) alert("Above Obesity"); */ 
+
+
+ // Challenge 3 // 
+ if (BMI >= 19 && BMI < 24 && age >= 19 && age < 24 ) {
+  alert("Healthy!");
+ } else if(BMI >= 20 && BMI < 25 && age > 25 && age < 34) {
+  alert("Healthy!");
+} else if(BMI >= 21 && BMI < 26 && age > 35 && age < 44) { 
+  alert("Healthy!");
+} else if(BMI >= 22 && BMI < 27 && age > 45 && age < 54) {
+  alert("Healthy!");
+} else if(BMI >= 23 && BMI < 28 && age > 55 && age < 64) { 
+  alert("Healthy!");
+} else if(BMI >= 24 && BMI < 29 && age > 65) { 
+alert("Healthy!");
+} else alert("Not Healthy!")
+} 
